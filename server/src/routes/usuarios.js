2a9c728de72516bab9b45af278c1234af6f0b8d9
Patrick Navega <patricknavega@MacBook-Air-de-Patrick.local>
2022-09-16 20:00:20 -0300

    criando rotas de usuarios

---
+++ server/src/routes/usuarios.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { listaUsuarios, cadastraUsuario, removeUsuario, atualizaUsuario } from "../controllers/usuarioController.js";
+
+const router = express.Router();
+
+router
+    .get('/usuarios',                   listaUsuarios   )
+    .post('/usuarios',                  cadastraUsuario )
+    .delete('/usuarios/:id',            removeUsuario   )
+    .patch('/usuarios/:id',             atualizaUsuario )
+
+export default router
