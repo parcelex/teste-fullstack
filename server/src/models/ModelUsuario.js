5783f01bf9a6930eebbb86af6b76491781a73056
Patrick da Silveira Navega <patrick.navega@guanabaraholding.com.br>
2022-09-26 16:59:47 -0300

    refatorando model server

--- server/src/models/ModelUsuario.js
+++ server/src/models/ModelUsuario.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const UsuarioSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     id:
             {
@@ -25,4 +25,4 @@ const UsuarioSchema = new mongoose.Schema(
   },
 );
 
-export default mongoose.model('usuarios', UsuarioSchema);
+export default mongoose.model('users', UserSchema);
